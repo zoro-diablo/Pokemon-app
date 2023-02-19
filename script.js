aa941f95dad97c49c63e9bc6446caffd3a9d003d
zoro-diablo <vyshnavk117@gmail.com>
2023-02-19 10:15:46 +0530

    Done

---
+++ script.js
@@ -0,0 +1,37 @@
+document.querySelector('#search').addEventListener('click', getPokemon)
+
+function capitalizeLetter(string){
+  return string.charAt(0).toUpperCase()+string.slice(1)
+}
+
+function lowerCaseName(string){
+  return string.toLowerCase()
+}
+
+function getPokemon(e) {
+  const name=document.querySelector('#pokemonName').value
+  const pokeName=lowerCaseName(name)
+
+  const pokiApi = fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+  pokiApi
+    .then((res) => res.json())
+    .then(
+      (data) =>
+        document.querySelector('.pokemonBox').innerHTML = `
+        <div>
+          <img
+            src="${data.sprites.other['official-artwork'].front_default}"
+            alt="${data.name}"
+          />
+        </div>
+        <div class="pokemonInfo">
+          <h1>${capitalizeLetter(data.name)}</h1>
+          <p>Weight : ${data.weight}</p>
+        </div>`
+    )
+    .catch((err) => console.log('Pokemon not Found', err))
+
+  e.preventDefault()
+}
+
+getPokemon()
